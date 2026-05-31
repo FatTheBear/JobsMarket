fab4c0eb7b12f4d7c6b64d9a54d9af7f824ad6cd
lehoangthong2412 <lehoangthong2412@gmail.com>
2026-05-31 15:30:13 +0700

    Resolve authRoutes conflict and clean up lockfiles

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -5,16 +5,7 @@ const { validateRegister } = require('../validators/authValidator');
 
 
 router.post('/login', authController.login);
-<<<<<<< HEAD
 router.post('/register', validateRegister, authController.register);
 router.post('/verify-otp', authController.verifyOTP);
-//cấm đụng vô đống này
-// const { verifyToken } = require('../middleware/authMiddleware');
-// const profileController = require('../controllers/profileController');
-// router.get('/profile', verifyToken, profileController.getProfile); 
-=======
-router.post('/register', authController.register);
-router.post('/login', authController.login);
->>>>>>> 5aeff53e32060a4c8e39ba0413a4180e6e1b404c
 
 module.exports = router;
