dd473606f7dcdb07a2ca82756cc6b4801bc6563e
lehoangthong2412 <lehoangthong2412@gmail.com>
2026-05-31 16:16:22 +0700

    Add job-posting and job-skills API routes, update server.js to include new routes

--- server/src/server.js
+++ server/src/server.js
@@ -13,9 +13,15 @@ require('./config/db');
 // Routes
 const authRoutes = require('./routes/authRoutes');
 const adminRoutes = require('./routes/adminRoutes');
+const companyRoutes = require('./routes/company');
+const jobsRoutes = require('./routes/jobs');
+const skillsRoutes = require('./routes/skills');
 
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
+app.use('/api/company', companyRoutes);
+app.use('/api/jobs', jobsRoutes);
+app.use('/api/skills', skillsRoutes);
 
 app.get("/", (req, res) => {
   res.send("JobsMarket API running...");
